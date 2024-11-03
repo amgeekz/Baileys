f5c389c24cb7458d6b53aa775306bf7b4c1f149c
amgeekz <79773348+amgeekz@users.noreply.github.com>
2024-11-03 11:19:49 +0700

    Update messages-send.ts

--- src/Socket/messages-send.ts
+++ src/Socket/messages-send.ts
@@ -783,16 +783,17 @@ export const makeMessagesSocket = (config: SocketConfig) => {
 						),
 						//TODO: CACHE
 						getProfilePicUrl: sock.profilePictureUrl,
-						let mediaHandle: string | undefined,
-				upload: async (readStream: Readable, opts: WAMediaUploadFunctionOpts) => { 
-					const up = await waUploadToServer(readStream, { ...opts, newsletter: isJidNewsletter(jid) });
-					if (up.handle) {
-						mediaHandle = up.handle;
+						upload: async (readStream: Readable, opts: WAMediaUploadFunctionOpts) => { 
+        const up = await waUploadToServer(readStream, { ...opts, newsletter: isJidNewsletter(jid) });
+        if (up.handle) {
+            mediaHandle = up.handle;
+        }
+        return up;
+    },
+						mediaCache: config.mediaCache,
+						options: config.options,
+						...options,
 					}
-					return up;
-				}
-					}
-				)
 				const isDeleteMsg = 'delete' in content && !!content.delete
 				const isEditMsg = 'edit' in content && !!content.edit
 				const isPinMsg = 'pin' in content && !!content.pin
